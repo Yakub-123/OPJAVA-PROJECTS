3788253268a82ef0f6bf0559b00a67db7090b539
Yakub-123 <venkysithub@gmail.com>
2024-07-11 15:47:39 +0530

    first commit is done

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Even
+{
+public static void main(String args[])
+{
+int n1=20;
+if()
+{
+System.out.println("The Number is EVEN");
+}
+}
+}
---
+++ Evan.java
@@ -0,0 +1,14 @@
+class Even
+{
+public static void main(String args[])
+{
+int n1=20;
+if(n1%2==0)
+{
+System.out.println("The Number is EVEN");
+}
+else
+{
+System.out.println("The Number is ODD");
+}
+}
